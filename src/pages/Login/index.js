c6d50fb3052d95013d6f56deea22e10617520d96
Alisson <alissontecnico@gmail.com>
2019-08-08 15:47:43 -0300

    add form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Title } from './styles';
+import logo from '../../assets/logo.svg';
 
-export default function Login() {
-  return <Title>Login</Title>;
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(username);
+
+    history.push('/main');
+  }
+
+  return (
+    <Title>
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          type="text"
+          placeholder="Digite seu usuário do Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </Title>
+  );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,5 +1,38 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  color: #7159c1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  form {
+    width: 100%;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  form input {
+    margin-top: 20px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    height: 48px;
+    padding: 0 20px;
+    font-size: 16px;
+    color: #6666;
+  }
+
+  form input::placeholder {
+    color: #999;
+  }
+
+  form button {
+    margin-top: 10px;
+    border: 0;
+    border-radius: 4px;
+    height: 48px;
+    font-size: 16px;
+    background: #df4723;
+  }
 `;
